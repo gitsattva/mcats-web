d24a3e0465a1433a582e8b0754dcac0b9c66b038
Ryan Massey <114211808+rsmassey@users.noreply.github.com>
2023-02-24 15:48:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,8 +108,8 @@ if model is not None:
     predictions_int = np.round(predictions).astype(int)
     pred = predictions_int.reshape(1,-1)
     pred = pred[0]
-    #max_val = max(pred)
+    max_val = max(pred)
     #max_val_index = pred.index(max_val)
     #genres = ['country', 'classical', 'metal', 'hiphop', 'electronic', 'pop', 'reggae', 'rock']
     #genre = genres[max_val_index]
-    st.write(f"The genre of this song is {pred}!")
+    st.write(f"The genre of this song is {max_val}!")
