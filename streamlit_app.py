55fd88c5dcf525d9a75769919bcf533d6d5c9fbe
Ryan Massey <114211808+rsmassey@users.noreply.github.com>
2023-02-25 11:38:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,4 +112,12 @@ if encoder is not None:
     st.markdown(f"<h1 style='text-align: center; color: red;'>{genre}</h1>", unsafe_allow_html=True)
     
     if genre == 'metal':
-        st.image('metal.gif', width=300)
+        file_ = open("metal.gif", "rb")
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+            unsafe_allow_html=True,
+        )
