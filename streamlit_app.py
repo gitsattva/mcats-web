fa6a979055b888627d28024f2d1b4b9e0316bf2e
Ryan Massey <114211808+rsmassey@users.noreply.github.com>
2023-02-25 16:37:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,4 +122,11 @@ try:
     )
     
 except NameError:
-    None
+    file_ = open('machine.gif', 'rb')
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode('utf-8')
+    file_.close()
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+        unsafe_allow_html=True,
+    )
