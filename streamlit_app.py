ef2e59480d24f1755e5ddb9dadb1f0436ea74d9a
Alexandre Bun <114267107+gitsattva@users.noreply.github.com>
2023-03-04 15:29:20 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,8 +120,14 @@ col1, col2 = st.columns([1, 1])
 
 with col1:
     music_file = st.file_uploader("Choose a music file")
-    with open(os.path.join("tempDir",music_file.name),"wb") as f:
-         f.write(music_file.getbuffer())
+    if music_file is not None:
+    file_details = {"FileName":music_file.name,"FileType":music_file.type}
+    st.write(file_details)
+    img = load_image(music_file)
+    st.image(img,height=250,width=250)
+    with open(os.path.join("tempDir",music_file.name),"wb") as f: 
+      f.write(music_file.getbuffer())         
+    st.success("Saved File")
 
     if music_file is not None:
         # audio_norm = normalize_volume(music_file)
